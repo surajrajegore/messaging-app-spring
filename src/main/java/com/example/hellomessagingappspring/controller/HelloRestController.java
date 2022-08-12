71c7318d3b3e3b00a1a1439b7152ee63f4e67464
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-12 16:36:56 +0530

    Added:rest controller

---
+++ src/main/java/com/example/hellomessagingappspring/controller/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.example.hellomessagingappspring.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hellomessage")
+public class HelloRestController {
+    @GetMapping(value = {"", "/", "/home"})
+    public String sayHello(){
+        return "Hello From Bridgelabz!";
+    }
+
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name){
+        return "Hello "+name+" From Bridgelabz";
+    }
+
+    @GetMapping("/param/{name}")
+    public String getName (@PathVariable String name) {
+        return "Hello "+name+" From Bridgelabz";    }
+}
