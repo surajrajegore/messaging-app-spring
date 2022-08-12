df44d005ea2b5dc0ca2c21983a1d10eccdfc2211
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-12 16:24:22 +0530

    Added:welcome message

---
+++ src/main/java/com/example/hellomessagingappspring/HelloMessagingAppSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.hellomessagingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppSpringApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome from bridgelabz");
+        SpringApplication.run(HelloMessagingAppSpringApplication.class, args);
+    }
+
+}
